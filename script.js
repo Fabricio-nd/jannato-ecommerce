7f1c5387ee542fbe1b9d02bb274f1d643e986b45
Fabricio-nd <nonatodamascenof@gmail.com>
2025-07-29 09:54:25 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
 const produtos = [
  {
-   nome: "vestido flor",
+   nome: "camisa polo",
    preco: 79.90,
-   imagem: "https://images.pexels.com/photos/33148751/pexels-photo-33148751.jpeg"
+   imagem: "https://media.istockphoto.com/id/696845268/pt/foto/white-polo-shirt-mock-up.jpg?s=2048x2048&w=is&k=20&c=F4jP00ONTvZVvNSXWDmJt4nUBS_nk3vhwDU7KS1k8kc="
  },
  {
-   nome: "vestido perola",
+   nome: "camisa social",
    preco: 99.90,
-   imagem: "https://images.pexels.com/photos/18377576/pexels-photo-18377576.jpeg"
+   imagem: "https://media.istockphoto.com/id/1208088017/pt/foto/business-or-white-blue-shirt-front-and-back-view-mock-up-isolated-on-white-background-with.jpg?s=2048x2048&w=is&k=20&c=qQxS5DzYhf_gHzqHq27d25FxvWYiSMMapPFOvYcJ8R8="
  },
  {
-   nome: "conjunto lilas",
+   nome: "calça alfaiataria",
    preco: 129.90,
-   imagem: "https://images.pexels.com/photos/17133981/pexels-photo-17133981.jpeg"
+   imagem: "https://media.istockphoto.com/id/1198732352/pt/foto/mens-blank-skinny-gray-jeans-template-from-two-sides-natural-shape-on-invisible-mannequin-for.jpg?s=2048x2048&w=is&k=20&c=DNgy7for9M-UN9P5TJy200HlEIKbMwV2MzQpNPXsjS0="
  },
  {
-   nome: "saia midi",
+   nome: "conjunto calça e camisa",
    preco: 149.90,
-   imagem: "https://images.pexels.com/photos/3952091/pexels-photo-3952091.jpeg"
+   imagem: "https://media.istockphoto.com/id/641443784/pt/foto/confiante-e-bonita.jpg?s=2048x2048&w=is&k=20&c=2RKI2N9pgR62QGJ9GY4axjGlxfvVlV-r0zkIqYzllHY="
  }
 ];
 const loginSection = document.getElementById("login-section");
@@ -75,9 +75,10 @@ function atualizarCarrinho() {
    carrinhoItems.appendChild(div);
    total += item.preco;
  });
- carrinhoTotal.textContent = `Total: R$ ${total.toFixed(2)}`;
+ carrinhoTotal.textContent = Total: R$ ${total.toFixed(2)};
 }
 campoBusca.addEventListener("input", () => {
  const termo = campoBusca.value.toLowerCase();
  const resultado = produtos.filter(p => p.nome.toLowerCase().includes(termo));
  mostrarProdutos(resultado);
+});
